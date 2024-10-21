d9c7456936efbb0d2c78e1bac656aef24eb4d1d4
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2024-10-21 20:14:10 +0500

    First commit

---
+++ src/test/java/me/zedaster/gptarticles/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zedaster.gptarticles.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
